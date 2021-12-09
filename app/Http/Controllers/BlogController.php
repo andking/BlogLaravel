e11f23d20b50971bbb220e8e57fa4ebc06792955
andking <yuschenko_andrey@ukr.net>
2021-12-08 23:40:32 -0500

    Pagination

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,7 +11,7 @@ class BlogController extends Controller
     public function index()
     {
         $categories = Category::orderby('title')->get();
-        $posts = Post::all();
+        $posts = Post::paginate(4);
         return view('pages.index', [
             'posts' => $posts,
             'categories' => $categories,
@@ -24,7 +24,7 @@ class BlogController extends Controller
         $current_category = Category::where('slug', $slug)->first();
 
         return view('pages.index', [
-            'posts' => $current_category->posts,
+            'posts' => $current_category->posts()->paginate(4),
             'categories' => $categories,
         ]);
     }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -22,15 +22,5 @@
             </div>
         </div>
     @endforeach
-
-    <nav aria-label="Page navigation example">
-        <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-        </ul>
-    </nav>
-
+    {{$posts->links('vendor.pagination.bootstrap-4')}}
 @endsection
